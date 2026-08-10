f096b46b62385cb8f87990a7fe5f96c23cacffa3
sibteh644-coder <sibteh644@gmail.com>
2026-08-10 00:20:01 -0700

    Update purge.js

--- src/commands/Moderation/purge.js
+++ src/commands/Moderation/purge.js
@@ -1,41 +1,25 @@
-import { SlashCommandBuilder, PermissionFlagsBits } from 'discord.js';
+import { SlashCommandBuilder } from 'discord.js';
 
 export default {
     data: new SlashCommandBuilder()
         .setName('purge')
-        .setDescription('Delete messages')
+        .setDescription('Purge messages')
         .addIntegerOption(option =>
             option
                 .setName('amount')
-                .setDescription('Number of messages to delete (1-100)')
+                .setDescription('Number of messages to purge')
                 .setRequired(true)
                 .setMinValue(1)
                 .setMaxValue(100)
-        )
-        .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages),
+        ),
 
-    async execute(interaction) {
+    prefixExecute: async (interaction) => {
         const amount = interaction.options.getInteger('amount');
 
-        const deleted = await interaction.channel.bulkDelete(amount, true);
-
-        await interaction.reply({
-            content: `Purged ${deleted.size} message${deleted.size === 1 ? '' : 's'}.`
-        });
-
-        setTimeout(() => {
-            interaction.deleteReply().catch(() => {});
-        }, 3000);
-    },
-
-    async prefixExecute(interaction) {
-        const amount = interaction.options.getInteger('amount');
-
-        if (!Number.isInteger(amount) || amount < 1 || amount > 100) {
-            await interaction.reply({
+        if (!amount || amount < 1 || amount > 100) {
+            return interaction.reply({
                 content: 'Please enter a number between 1 and 100.'
             });
-            return;
         }
 
         try {
@@ -53,7 +37,7 @@ export default {
             console.error('Purge error:', error);
 
             await interaction.reply({
-                content: 'I cannot delete messages here. Make sure I have the **Manage Messages** permission.'
+                content: 'I cannot delete messages here. Make sure I have Manage Messages permission.'
             });
         }
     }
