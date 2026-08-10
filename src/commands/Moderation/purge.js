fc045fb0b04be04fde1ed30982a6dad4397dc5b4
sibteh644-coder <sibteh644@gmail.com>
2026-08-10 00:29:02 -0700

    Update purge.js

--- src/commands/Moderation/purge.js
+++ src/commands/Moderation/purge.js
@@ -13,16 +13,8 @@ export default {
                 .setMaxValue(100)
         ),
 
-    prefixOnly: true,
-
-    async prefixExecute(interaction) {
+    async execute(interaction) {
         const amount = interaction.options.getInteger('amount');
-
-        if (!amount || amount < 1 || amount > 100) {
-            return interaction.reply({
-                content: 'Please enter a number between 1 and 100.'
-            });
-        }
 
         if (!interaction.member.permissions.has(PermissionFlagsBits.ManageMessages)) {
             return interaction.reply({
@@ -45,7 +37,36 @@ export default {
             console.error('Purge error:', error);
 
             await interaction.reply({
-                content: 'I could not delete those messages. Make sure I have **Manage Messages** permission.'
+                content: 'I could not delete those messages.'
+            });
+        }
+    },
+
+    async prefixExecute(interaction) {
+        const amount = interaction.options.getInteger('amount');
+
+        if (!interaction.member.permissions.has(PermissionFlagsBits.ManageMessages)) {
+            return interaction.reply({
+                content: 'You need the **Manage Messages** permission to use this command.'
+            });
+        }
+
+        try {
+            const deleted = await interaction.channel.bulkDelete(amount, true);
+
+            await interaction.reply({
+                content: `Purged ${deleted.size} message${deleted.size === 1 ? '' : 's'}.`
+            });
+
+            setTimeout(() => {
+                interaction.deleteReply().catch(() => {});
+            }, 3000);
+
+        } catch (error) {
+            console.error('Purge error:', error);
+
+            await interaction.reply({
+                content: 'I could not delete those messages.'
             });
         }
     }
