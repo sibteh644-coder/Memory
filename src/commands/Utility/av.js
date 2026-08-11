410ed4312a819b7741aaa2b6cc737bdad9031046
sibteh644-coder <sibteh644@gmail.com>
2026-08-11 01:51:11 -0700

    Update av.js

--- src/commands/Utility/av.js
+++ src/commands/Utility/av.js
@@ -1,50 +1,57 @@
 import { SlashCommandBuilder, EmbedBuilder } from 'discord.js';
 
 export default {
-data: new SlashCommandBuilder()
-.setName('av')
-.setDescription("Show a user's avatar")
-.addUserOption(option =>
-option
-.setName('user')
-.setDescription('The user whose avatar you want to see')
-.setRequired(false)
-),
+    data: new SlashCommandBuilder()
+        .setName('av')
+        .setDescription("Show a user's avatar")
+        .addUserOption(option =>
+            option
+                .setName('user')
+                .setDescription('The user whose avatar you want to see')
+                .setRequired(false)
+        ),
 
-```
-async execute(interaction) {
-    const user = interaction.options.getUser('user') || interaction.user;
+    // /av
+    async execute(interaction) {
+        const user = interaction.options.getUser('user') || interaction.user;
 
-    const avatar = user.displayAvatarURL({
-        size: 4096,
-        dynamic: true
-    });
+        const avatar = user.displayAvatarURL({
+            size: 4096,
+            dynamic: true
+        });
 
-    const embed = new EmbedBuilder()
-        .setTitle(`${user.username}'s Avatar`)
-        .setImage(avatar)
-        .setURL(avatar)
-        .setFooter({ text: `Requested by ${interaction.user.username}` });
+        const embed = new EmbedBuilder()
+            .setTitle(`${user.username}'s Avatar`)
+            .setImage(avatar)
+            .setURL(avatar)
+            .setFooter({
+                text: `Requested by ${interaction.user.username}`
+            });
 
-    await interaction.reply({ embeds: [embed] });
-},
+        await interaction.reply({
+            embeds: [embed]
+        });
+    },
 
-async prefixExecute(message) {
-    const user = message.mentions.users.first() || message.author;
+    // ?av
+    async prefixExecute(message, args) {
+        const user = message.mentions.users.first() || message.author;
 
-    const avatar = user.displayAvatarURL({
-        size: 4096,
-        dynamic: true
-    });
+        const avatar = user.displayAvatarURL({
+            size: 4096,
+            dynamic: true
+        });
 
-    const embed = new EmbedBuilder()
-        .setTitle(`${user.username}'s Avatar`)
-        .setImage(avatar)
-        .setURL(avatar)
-        .setFooter({ text: `Requested by ${message.author.username}` });
+        const embed = new EmbedBuilder()
+            .setTitle(`${user.username}'s Avatar`)
+            .setImage(avatar)
+            .setURL(avatar)
+            .setFooter({
+                text: `Requested by ${message.author.username}`
+            });
 
-    await message.reply({ embeds: [embed] });
-}
-```
-
+        await message.reply({
+            embeds: [embed]
+        });
+    }
 };
